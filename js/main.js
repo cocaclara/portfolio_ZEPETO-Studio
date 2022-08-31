4cd018f412964954c275d08831cf24b14dd4eba3
Clara <107092024+cocaclara@users.noreply.github.com>
2022-08-31 19:19:50 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 "use strict";
 
 $(document).ready(function () {
-
+  // .lnb li 마우스 오버(아웃) 시
+  $('.lnb').mouseover(function(){
+    $(this).siblings('a').css('color','#fff');
+  });
+  $('.lnb').mouseout(function(){
+    $(this).siblings('a').css('color','#bababa');
+  });
 });
