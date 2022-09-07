f857ceee897f1ce46a9acdd5f97ac52b4667e662
Clara <107092024+cocaclara@users.noreply.github.com>
2022-09-07 17:41:17 +0900

    second commit

--- js/swiper.js
+++ js/swiper.js
@@ -1,24 +1,51 @@
-// Swiper - Autoplay, Infinite loop
+// Swiper - Autoplay, Infinite loop, Responsive breakpoints
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 5,
   spaceBetween: 20,
-  loop: true,
-  centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+  loop: true,               // 슬라이드 반복
+  centeredSlides: true,     // 슬라이트 가운데 배치
+  
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+      spaceBetween: 50,
     },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1280: {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    },
+    zoom: {
+      maxRatio: 2,
+      minRatio: 1
+    },
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
 });
 
 $(document).ready(function(){
-  // $('.swiper-slide-active').;
+    // $('.swiper-slide-active').find('.profile-box').show();
 });
