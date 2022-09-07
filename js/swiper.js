7b34b5a051a82129354bd1e97a025cfccd8d330f
Clara <107092024+cocaclara@users.noreply.github.com>
2022-09-07 18:25:09 +0900

    second commit

--- js/swiper.js
+++ js/swiper.js
@@ -5,11 +5,10 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 20,
   loop: true,               // 슬라이드 반복
   centeredSlides: true,     // 슬라이트 가운데 배치
-  
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   breakpoints: {
     0: {
       slidesPerView: 3,
@@ -27,23 +26,11 @@ var swiper = new Swiper(".mySwiper", {
       slidesPerView: 4,
       spaceBetween: 30,
     },
-    1280: {
+    1440: {
       slidesPerView: 5,
       spaceBetween: 40,
     },
-    zoom: {
-      maxRatio: 2,
-      minRatio: 1
-    },
   },
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev",
-  // },
 });
 
 $(document).ready(function(){
